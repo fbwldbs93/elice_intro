6e5a6a5054076c8905385aec922ec02916159b02
jiyun <fbwldbs93@naver.com>
2022-07-08 18:37:42 +0900

    add js

--- script.js
+++ script.js
@@ -7,19 +7,49 @@ let eachNav;
 
 for(let i=0; i <section.length; i++ ){
     eachSection = section[i]
-    eachSection.style.display="none";
-    section[0].style.display="block";
+    eachSection.classList.add('hide')
+    section[0].classList.add('show')
+
+    // console.log(Array.from(section).indexOf(section[0]))
 }
 
 
-
-for(let i=0; i <nav.length; i++ ){
-
-    nav[i].addEventListener("click", function openContent(){
-        if(section[i]){
-            section[i].style.display="block";
+nav.forEach((curEl, index, array)=>{
+    curEl.addEventListener('click', function(){ 
+        for(let i =0; i <array.length; i ++ ){
+            section[i].classList.remove('show')
+            section[i].classList.add('hide')
         }
         
+
+        section[index].classList.remove('hide')
+        section[index].classList.add('show')
     })
-}
+})
+
+
+
+// for(let i=0; i <nav.length; i++ ){
+
+//     nav[i].addEventListener("click", function openContent(e){
+
+//         // console.log(Array.from(nav).indexOf(e.currentTarget))
+
+//         let cur = Array.from(nav).indexOf(e.currentTarget)
+
+//         let secCur = Array.from(section).indexOf(section[i])
+
+
+//         if(cur == secCur){
+//             section[i].classList.remove('show')
+//             section[i].classList.add('hide')
+//             section[cur].classList.remove('hide')
+//             section[cur].classList.add('show')
+
+//         }
+        
+
+        
+//     })
+// }
 
